90b0feca5f6085fc11623488263ca25cc5a63fc1
Yussef Raouf Abdelmisih <43096356+yusseef@users.noreply.github.com>
2018-11-23 03:14:13 -0500

    Create 1012 beginner.py

---
+++ 1012 beginner.py
@@ -0,0 +1,14 @@
+a,b,c=input().split(" ")
+a=float(a)
+b=float(b)
+c=float(c)
+triangulo=(a*c)/2
+circulo=c*c*3.14159
+trapezio=((a+b)/2)*c
+quadrado=b*b
+retangulo=a*b
+print("TRIANGULO: %.3f" %triangulo);
+print("CIRCULO: %.3f" %circulo)
+print("TRAPEZIO: %.3f" %trapezio)
+print("QUADRADO: %.3f" %quadrado)
+print("RETANGULO: %.3f" %retangulo)
