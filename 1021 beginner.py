79defb17ddac495fc6dd610810627ffa0256615d
Yussef Raouf Abdelmisih <43096356+yusseef@users.noreply.github.com>
2019-02-06 19:29:33 -0800

    Create 1021 beginner.py

---
+++ 1021 beginner.py
@@ -0,0 +1,47 @@
+i=float(input())
+(result1)=i//100
+trash1=i%100
+result2=trash1//50
+trash2=trash1%50
+result3=trash2//20
+trash3=trash2%20
+result4=trash3//10
+trash4=trash3%10
+result5=trash4//5
+trash5=trash4%5
+result6=trash5//2
+trash6=trash5%2
+result7=trash6//1
+trash7=trash6%1
+result8=trash7//0.50
+trash8=trash7%0.50
+result9=trash8//0.25
+trash9=trash8%0.25
+result10=trash9//0.10
+trash10=trash9%0.10
+result11=trash10//0.05
+trash11=trash10%0.05
+result12=trash11//0.01
+print("NOTAS:")
+print(int(result1),"nota(s) de R$ 100.00")
+print(int(result2),"nota(s) de R$ 50.00")
+print(int(result3),"nota(s) de R$ 20.00")
+print(int(result4),"nota(s) de R$ 10.00")
+print(int(result5),"nota(s) de R$ 5.0")
+print(int(result6),"nota(s) de R$ 2.0")
+print("MOEDAS:")
+print(int(result7),"moeda(s) de R$ 1.00")
+print(int(result8),"moeda(s) de R$ 0.50")
+print(int(result9),"moeda(s) de R$ 0.25")
+print(int(result10),"moeda(s) de R$ 0.10")
+print(int(result11),"moeda(s) de R$ 0.05")
+print(int(result12),"moeda(s) de R$ 0.01")
+
+
+
+
+
+
+
+
+
