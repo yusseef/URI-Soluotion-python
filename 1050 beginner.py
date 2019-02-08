6d344e895b1b023b265937477b99f2c724164088
Yussef Raouf Abdelmisih <43096356+yusseef@users.noreply.github.com>
2019-02-08 10:04:43 -0800

    Create 1050 beginner.py

---
+++ 1050 beginner.py
@@ -0,0 +1,19 @@
+i=int(input())
+if i==61:
+	print("Brasilia")
+elif i==71:
+	print("Salvador")
+elif i==11:
+	print("Sao Paulo")
+elif i==21:
+	print("Rio de Janeiro")
+elif i==32:
+	print("Juiz de Fora")
+elif i==19:
+	print("Campinas")
+elif i==27:
+	print("Vitoria")
+elif i==31:
+	print("Belo Horizonte")
+else:
+	print("DDD nao cadastrado")
