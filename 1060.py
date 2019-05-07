ef1f9616b48f069d0060dd8fd364980964c5bcbf
Yussef Raouf Abdelmisih <43096356+yusseef@users.noreply.github.com>
2019-05-06 19:22:50 -0700

    Create 1060.py

---
+++ 1060.py
@@ -0,0 +1,20 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
+f = float(input())
+even = 0
+if a>0:
+    even+=1
+if b>0:
+    even+=1
+if c>0:
+    even+=1
+if d>0:
+    even+=1
+if e>0:
+    even+=1
+if f>0:
+    even+=1
+print(even,"valores positivos")
